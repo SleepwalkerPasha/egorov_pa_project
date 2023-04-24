8ec5681b6ab8313fc22d040ec7714b952d41deda
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-04-24 12:50:35 +0300

    optimize imports

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/repository/jpa/interfaces/JpaLinkDao.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/repository/jpa/interfaces/JpaLinkDao.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.tinkoff.edu.java.scrapper.entity.LinkEntity;
 
-import java.net.URI;
 import java.time.OffsetDateTime;
 import java.util.Collection;
 import java.util.Optional;
--- scrapper/src/test/java/IntegrationEnvironment.java
+++ scrapper/src/test/java/IntegrationEnvironment.java
@@ -1,12 +1,10 @@
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionManager;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
