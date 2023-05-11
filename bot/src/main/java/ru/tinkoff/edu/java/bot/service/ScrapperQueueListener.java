003b2a64b867107f8f749eb1f3944ca5c289e804
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-05-11 09:25:37 +0300

    handle error messages

--- bot/src/main/java/ru/tinkoff/edu/java/bot/service/ScrapperQueueListener.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/service/ScrapperQueueListener.java
@@ -1,11 +1,14 @@
 package ru.tinkoff.edu.java.bot.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import ru.tinkoff.edu.java.common.dto.request.LinkUpdateRequest;
 
 @RabbitListener(queues = "${app.queue-name}")
+@Slf4j
 @RequiredArgsConstructor
 public class ScrapperQueueListener implements Listener {
 
@@ -15,6 +18,11 @@ public class ScrapperQueueListener implements Listener {
     @RabbitHandler
     public void receiveNotification(LinkUpdateRequest request) {
         handler.handleUpdates(request);
+    }
+
+    @RabbitListener(queues = "${app.queue-name}" + ".dlq")
+    public void processFailedMessages(Message message) {
+        log.info("Received failed message: {}", message.toString());
     }
 
 }
